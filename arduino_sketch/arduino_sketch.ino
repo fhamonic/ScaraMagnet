df006a3b508e6d8de99754e67db319e0b3be1ac1
fhamonic <francois.hamonic@gmail.com>
2023-10-10 01:47:41 +0200

    inverse kinematics

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -3,9 +3,9 @@
 using Timer1 = ATMEGA328P::Timer1<C2MHz>;
 using Timer2 = ATMEGA328P::Timer2<C15_625Hz>;
 
-using Arm1Stepper = Stepper<10,9, 800, 4, 2>;
-using Arm2Stepper = Stepper<8,5, 800, 4, 2>;
-using ZStepper = Stepper<6,7, 1600, 1, 3>;
+using Arm1Stepper = Stepper<10, 9, 800, 4, 2>;
+using Arm2Stepper = Stepper<8, 5, 800, 4, 2>;
+using ZStepper = Stepper<6, 7, 1600, 1, 3>;
 
 using Arm1Profile = TrapezoidalProfile<Arm1Stepper, Timer1::CounterA>;
 using Arm2Profile = TrapezoidalProfile<Arm2Stepper, Timer1::CounterB>;
@@ -15,80 +15,102 @@ ISR(TIMER1_COMPA_vect) { Arm1Profile::DoStep(); }
 ISR(TIMER1_COMPB_vect) { Arm2Profile::DoStep(); }
 ISR(TIMER2_COMPA_vect) { ZProfile::DoStep(); }
 
-
-#define INPUT_BUFFER_SIZE 64
-static char inputBuffer[INPUT_BUFFER_SIZE];
-
-
-// sudo chmod a+rw /dev/ttyACM0
-
-
 void setup() {
     Arm1Stepper::Setup();
     Arm2Stepper::Setup();
     ZStepper::Setup();
-    
+
     Timer1::Setup();
     Timer2::Setup();
-    
+
     Arm1Profile::Setup();
     Arm2Profile::Setup();
     ZProfile::Setup();
 
     Serial.begin(19200);
     pinModeFast(2, INPUT_PULLUP);
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(3, OUTPUT);
+    Serial.println("Hello");
 }
 
+void loop() { handleSerialInput(); }
 
-
-void loop() { 
-  
+void pickOrPlace(bool pick) {
     ZProfile::SetSpeed(-1.0);
     while(digitalReadFast(2) != LOW) delayMicroseconds(100);
     ZProfile::SetSpeed(0.0);
-    while(ZProfile::IsMoving());
+    digitalWrite(3, pick ? HIGH : LOW);
+    ZProfile::WaitStop();
     ZProfile::MoveForward(-ZStepper::stepPos);
-    
-    
-  while(true) {
-    
-    /*
-    Arm1Profile::MoveForward(1600);
-    Arm2Profile::MoveBackward(1600);
-    
-    ZProfile::MoveForward(4600);
-    
-    while(Arm1Profile::IsMoving() || Arm2Profile::IsMoving() || ZProfile::IsMoving());
-    Arm1Profile::MoveBackward(1600);
-    Arm2Profile::MoveForward(1600);
-    ZProfile::MoveBackward(4600);
-    while(Arm1Profile::IsMoving() || Arm2Profile::IsMoving() || ZProfile::IsMoving());
-*/
-    
-  }
-  //handleSerialInput(); 
+    ZProfile::WaitStop();
+}
 
+//     /\
+//    A  B
+//   /    \
+//   ---C---
+// This function returns the angle between A and B in radians
+float lawOfCosines(float a, float b, float c) {
+    return acosf((a * a + b * b - c * c) / (2.0f * a * b));
+}
+//               /
+//              / \  
+//             /   A2
+//            /     )
+//      |-A1-o----------O
+//      |   /
+//      |  /
+//      | /
+//      |/
+//     / \
+//    /   \
+//   /     \
+//   -------
+// distances in mm, angles in degrees
+void InverseKinematic(float x, float y, float * A0, float * A1, float * A2) {
+    static const float rad2deg = 57.2958f;
+    static const float arm1_len = 91.61f;
+    static const float arm2_len = 105.92f;
+
+    float dist = sqrt(x * x + y * y);
+
+    if(dist > (arm1_len + arm2_len)) {
+        dist = (arm1_len + arm2_len) - 0.001f;
+        Serial.println("IK overflow->limit");
+    }
+    *A0 = atan2(y, x) * rad2deg;
+    *A1 = lawOfCosines(dist, arm1_len, arm2_len) * rad2deg;
+    *A2 = 180.0f - lawOfCosines(arm1_len, arm2_len, dist) * rad2deg;
+}
+
+// InverseKinematic(x, y, 91.61, 105.92, elbow, &A1, &A2);
+
+void moveTo(float x, float y) {
+    static bool elbow_left = true;
+    float A0, A1, A2;
+    InverseKinematic(x, y, &A0, &A1, &A2);
+
+    Serial.println(A1);
+    Serial.println(A2);
+
+    // Arm1Profile::MoveTo(800 * (A0+A1)/360.0);
+    // Arm2Profile::MoveTo(800 * (A2)/360.0);
+    // Arm1Profile::WaitStop();
+    // Arm2Profile::WaitStop();
 }
 
 void handleSerialInput() {
     if(!Serial.available()) return;
-    size_t byte_count =
-        Serial.readBytesUntil('\n', inputBuffer, INPUT_BUFFER_SIZE);
-    inputBuffer[byte_count] = '\0';
-    interpretCommand(inputBuffer, byte_count);
-}
-
-void interpretCommand(char * inputBuffer, byte commandLength) {
-    if(inputBuffer[0] == 'P') {
-    } else if(inputBuffer[0] == '0') {
-        Serial.println("OK");
-    } else if(inputBuffer[0] == '1') {
-        Serial.println("OK");
-    } else {
-        Serial.print("Unknown command: '");
-        Serial.print(inputBuffer);
-        Serial.println("'");
+    char c = '\0';
+    Serial.readBytes(&c, 1);
+    if(c == 'G') {
+        pickOrPlace(true);
+    } else if(c == 'R') {
+        pickOrPlace(false);
+    } else if(c == 'M') {
+        float x = Serial.parseFloat();
+        float y = Serial.parseFloat();
+        moveTo(x, y);
     }
-    Serial.flush();
+    Serial.readStringUntil('\n');
 }
